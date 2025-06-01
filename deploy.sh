57c7823e0d928c755921c1c16756cdabbc4b4ea5
Sreejesh T <sreejesh.t@gmail.com>
2025-06-01 21:14:41 +0000

    Jenkins-file update

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,10 @@
 echo "🚀 Deploying React app container..."
 
 # Stop and remove existing container if it exists
-docker stop react-app 2>/dev/null && docker rm react-app 2>/dev/null
-
+docker stop react-app
+docker rm react-app
 # Run new container on devops-net with port 80 exposed
-docker run -d \
-  --name react-app \
-  --network devops-net \
-  -p 80:80 \
-  react-static-app
+docker run -d --name react-app   --network devops-net   -p 80:80   react-static-app
 
 # Confirm deployment
 echo "✅ react-app container deployed and running on http://localhost:80"
